4d8be06fcdd7efcf963e9042e49d705b7dc5419d
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-08-07 21:39:18 +0530

    Completed all CRUD operations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -93,6 +93,21 @@ const Mutation = {
         // console.log(post)
         return deletedPosts[0]
     },
+    updatePost(parent, args, { db }, info){
+        const post = db.posts.find((pst) => pst.id === args.id)
+        if(!post){
+            throw new Error('Could Not Find Post')
+        }
+
+        if(typeof args.data.title === 'string'){
+            post.title = args.data.title 
+        }
+
+        if(typeof args.data.body === 'string'){
+            post.body = args.data.body
+        }
+        return post
+    },
     createComment(parent, args, { db }, info){
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post)
@@ -113,6 +128,15 @@ const Mutation = {
         }
         const deletedComments = db.comments.splice(commentIndex, 1)
         return deletedComments[0]
+    },updateComment(parent, args, { db }, info){
+        const comment = db.comments.find((comment) => comment.id === args.id)
+        if(!comment){
+            throw new Error('Sorry, Comment Not Found!')
+        }
+        if(typeof args.data.text === 'string'){
+            comment.text = args.data.text
+        }
+        return comment
     }
 }
 
