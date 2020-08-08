c53c66dd3a87a2e313d8ac4174a72cf0a106da94
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-08-08 23:02:21 +0530

    Testing Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql, PubSub } from 'apollo-server'
 var fs = require('fs');
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import Post from './resolvers/Post'
 import User from './resolvers/User'
 import Comment from './resolvers/Comment'
 import { merge } from 'lodash'
 
+const pubsub = new PubSub()
+
 const server = new ApolloServer({
     typeDefs: gql`${fs.readFileSync('./src/schema.graphql')}`,
-    resolvers: { Query, Mutation, Post, User, Comment },
+    resolvers: { Query, Mutation, Subscription, Post, User, Comment },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 
